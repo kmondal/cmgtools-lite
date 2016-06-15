be95c31a3d1284e5acc9e0d6b9f38e976472c8ff
Pietro Vischia <pietro.vischia@gmail.com>
2016-06-15 10:05:40 +0200

    First commit for tau ID and FR studies (for 2l1tau and 1l2taus final states)

--- TTHAnalysis/python/plotter/susy-multilepton/2l1tau/preliminaryFRstudy_steer.sh
+++ TTHAnalysis/python/plotter/susy-multilepton/2l1tau/preliminaryFRstudy_steer.sh
@@ -18,20 +18,36 @@ NPROC=8
 
 LUMI=10.0
 
+
 if [ "${1}" = "anal" ]; then
 
     python ${ANALYZER} ${MCA} ${CUTS} --path ${TREESPATH} --tree ${TREE}  --lumi ${LUMI} --s2v -j ${NPROC} --Fs ${TREESPATH}leptonJetReCleanerSusyRA7mva --Fs ${TREESPATH}leptonChoiceEWK --mcc ${MCC} -f -G
 
 elif [ "${1}" = "plot" ]; then
 
+    rm -r ${PLOTSDIR}TT/
+    rm -r ${PLOTSDIR}DY/
+    rm -r ${PLOTSDIR}data/
+    
+    mkdir -p ${PLOTSDIR}TT/
+    mkdir -p ${PLOTSDIR}DY/
+    mkdir -p ${PLOTSDIR}data/
+
+    cp ${PLOTSDIR}index.php ${PLOTSDIR}
+    cp ${PLOTSDIR}index.php ${PLOTSDIR}TT/
+    cp ${PLOTSDIR}index.php ${PLOTSDIR}DY/
+    cp ${PLOTSDIR}index.php ${PLOTSDIR}data/
+    
     # TT
-    python ${PLOTTER} --exclude-process data --exclude-process DY ${MCA} ${CUTS} ${PLOTS} --path ${TREESPATH} --tree ${TREE} --lumi ${LUMI} --s2v -j ${NPROC} --Fs ${TREESPATH}leptonJetReCleanerSusyRA7mva --Fs ${TREESPATH}leptonChoiceEWK -mcc ${MCC} -f --rspam "%(lumi) (13 TeV)" --lspam "#bf{CMS} #it{Preliminary}" --legendBorder=0 --legendFontSize 0.055 --legendWidth=0.30 --showMCError --pdir ${PLOTSDIR}
+    python ${PLOTTER} --exclude-process data --exclude-process DY ${MCA} ${CUTS} ${PLOTS} --path ${TREESPATH} --tree ${TREE} --lumi ${LUMI} --s2v -j ${NPROC} --Fs ${TREESPATH}leptonJetReCleanerSusyRA7mva --Fs ${TREESPATH}leptonChoiceEWK --mcc ${MCC} --rspam "%(lumi) (13 TeV)" --lspam "#bf{CMS} #it{Preliminary}" --legendBorder=0 --legendFontSize 0.055 --legendWidth=0.30 --showMCError --pdir ${PLOTSDIR}TT/ &
 
     # DY
-    python ${PLOTTER} --exclude-process data --exclude-process TT ${MCA} ${CUTS} ${PLOTS} --path ${TREESPATH} --tree ${TREE} --lumi ${LUMI} --s2v -j ${NPROC} --Fs ${TREESPATH}leptonJetReCleanerSusyRA7mva --Fs ${TREESPATH}leptonChoiceEWK -mcc ${MCC} -f --rspam "%(lumi) (13 TeV)" --lspam "#bf{CMS} #it{Preliminary}" --legendBorder=0 --legendFontSize 0.055 --legendWidth=0.30 --showMCError --pdir ${PLOTSDIR}
+    python ${PLOTTER} --exclude-process data --exclude-process TT ${MCA} ${CUTS} ${PLOTS} --path ${TREESPATH} --tree ${TREE} --lumi ${LUMI} --s2v -j ${NPROC} --Fs ${TREESPATH}leptonJetReCleanerSusyRA7mva --Fs ${TREESPATH}leptonChoiceEWK --mcc ${MCC} --rspam "%(lumi) (13 TeV)" --lspam "#bf{CMS} #it{Preliminary}" --legendBorder=0 --legendFontSize 0.055 --legendWidth=0.30 --showMCError --pdir ${PLOTSDIR}DY/ &
 
     # Data
-    python ${PLOTTER} --exclude-process TT --exclude-process DY ${MCA} ${CUTS} ${PLOTS} --path ${TREESPATH} --tree ${TREE} --lumi ${LUMI} --s2v -j ${NPROC} --Fs ${TREESPATH}leptonJetReCleanerSusyRA7mva --Fs ${TREESPATH}leptonChoiceEWK -mcc ${MCC} -f --rspam "%(lumi) (13 TeV)" --lspam "#bf{CMS} #it{Preliminary}" --legendBorder=0 --legendFontSize 0.055 --legendWidth=0.30 --showMCError --pdir ${PLOTSDIR}
+    python ${PLOTTER} --exclude-process TT --exclude-process DY ${MCA} ${CUTS} ${PLOTS} --path ${TREESPATH} --tree ${TREE} --lumi ${LUMI} --s2v -j ${NPROC} --Fs ${TREESPATH}leptonJetReCleanerSusyRA7mva --Fs ${TREESPATH}leptonChoiceEWK --mcc ${MCC} --rspam "%(lumi) (13 TeV)" --lspam "#bf{CMS} #it{Preliminary}" --legendBorder=0 --legendFontSize 0.055 --legendWidth=0.30 --showMCError --pdir ${PLOTSDIR}data/ &
+
+
 fi
 
 
