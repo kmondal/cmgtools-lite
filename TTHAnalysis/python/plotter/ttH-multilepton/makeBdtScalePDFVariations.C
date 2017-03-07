eacbd2b25112efe0707f36e6f71a6cd9b77bfc4d
Pietro Vischia <pietro.vischia@gmail.com>
2017-03-07 12:48:48 +0100

    Add 3l-nomem, and added preliminary plots to understand scale variations

--- TTHAnalysis/python/plotter/ttH-multilepton/makeBdtScalePDFVariations.C
+++ TTHAnalysis/python/plotter/ttH-multilepton/makeBdtScalePDFVariations.C
@@ -10,7 +10,7 @@ Double_t fitRatio(TH1* h1, TH1*h2)
   return r;
 }
 
-void makeBdtScalePDFVariations(TString dir, TString region, bool savePdfReplicas=false){
+void makeBdtScalePDFVariations(TString dir, TString region, bool savePdfReplicas=false, bool doScaleStudy=false){
 
   TFile* f = TFile::Open(Form("%s/%s/2lss_3l_plots.root",dir.Data(),region.Data()), "READ");
   
@@ -24,7 +24,7 @@ void makeBdtScalePDFVariations(TString dir, TString region, bool savePdfReplicas
   else if(region.Contains("3l"))
     {
       variable.push_back("kinMVA_3l_ttbar");
-      variable.push_back("kinMVA_3l_ttV_withMEM");
+      variable.push_back( region.Contains("prescale") ? "kinMVA_3l_ttV_withMEM" : "kinMVA_3l_ttV");
     }
   
   vector<TString> proc;
@@ -35,11 +35,17 @@ void makeBdtScalePDFVariations(TString dir, TString region, bool savePdfReplicas
   for(int iVariable=0;iVariable<2; ++iVariable)
     {
       cout << "BDT: " << variable[iVariable] << endl;
-      TFile* out = NULL;
+      TFile* outPdf = NULL;
+      TFile* outScale = NULL;
       if(savePdfReplicas)
         {
-          out = TFile::Open(Form("%s_pdfVars.root",f->GetName()), "RECREATE");
-          cout << "Distribution of replicas will be saved in: " << out->GetName() << endl;
+          outPdf = TFile::Open(Form("%s_pdfVars.root",f->GetName()), "RECREATE");
+          cout << "Distribution of replicas will be saved in: " << outPdf->GetName() << endl;
+        }
+      if(doScaleStudy)
+        {
+          outScale = TFile::Open(Form("%s_scaleVars.root",f->GetName()), "RECREATE");
+          cout << "Plot for scale variation studies will be saved in: " << outScale->GetName() << endl;
         }
       
       for(int iproc=0; iproc<3; ++iproc)
@@ -61,13 +67,15 @@ void makeBdtScalePDFVariations(TString dir, TString region, bool savePdfReplicas
           cout << h->GetRMS() << endl;
           if(savePdfReplicas)
             {
-              out->cd();
+              outPdf->cd();
               h->Write();
             }
           delete h;
 
           // SCALE
           cout << "=========== SCALE: " << endl;
+          TGraph* gr = new TGraph(8);
+          gr->SetName(Form("%s_%s",variable[iVariable].Data(),proc[iproc].Data()));
           double max(0.);
           for(int ivar=1; ivar<9; ++ivar)
             {
@@ -75,9 +83,20 @@ void makeBdtScalePDFVariations(TString dir, TString region, bool savePdfReplicas
               double r(fitRatio(var, nominal));
               //cout << r << endl;
               if(r>max) max=r;
+              gr->SetPoint(ivar-1,ivar,r);
             }
           cout << max << endl;
+          if(doScaleStudy)
+            {
+              outScale->cd();
+              gr->Write();
+            }
+
         }
+      
+      if(outPdf)   outPdf  ->Close();
+      if(outScale) outScale->Close();
+
     }
   gApplication->Terminate();
 }
