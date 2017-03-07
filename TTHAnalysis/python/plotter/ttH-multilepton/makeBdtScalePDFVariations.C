55b09d8a1453325c97e02274e31f1410366056ec
Pietro Vischia <pietro.vischia@gmail.com>
2017-03-07 16:11:01 +0100

    Fancier graphics

--- TTHAnalysis/python/plotter/ttH-multilepton/makeBdtScalePDFVariations.C
+++ TTHAnalysis/python/plotter/ttH-multilepton/makeBdtScalePDFVariations.C
@@ -11,7 +11,7 @@ Double_t fitRatio(TH1* h1, TH1*h2, double& err)
   return r;
 }
 
-void makeBdtScalePDFVariations(TString dir, TString region, bool savePdfReplicas=false, bool doScaleStudy=false){
+void makeBdtScalePDFVariations(TString dir, TString region, bool savePdfReplicas=false, bool doScaleStudy=false, bool doFigures=false){
 
   TFile* f = TFile::Open(Form("%s/%s/2lss_3l_plots.root",dir.Data(),region.Data()), "READ");
   
@@ -71,23 +71,38 @@ void makeBdtScalePDFVariations(TString dir, TString region, bool savePdfReplicas
             {
               outPdf->cd();
               h->Write();
+              if(doFigures)
+                {
+                  TCanvas* c = new TCanvas("c","c", 800,800);
+                  c->cd();
+                  h->Draw();
+                  c->Print(Form("%s/%s/%s_nnpdfVar.png",dir.Data(),region.Data(),h->GetName()));
+                  c->Print(Form("%s/%s/%s_nnpdfVar.pdf",dir.Data(),region.Data(),h->GetName()));
+                  delete c;
+                }
+
             }
           delete h;
 
           // SCALE
           cout << "=========== SCALE: " << endl;
           TH1D* gr = new TH1D(Form("%s_%s",variable[iVariable].Data(),proc[iproc].Data()), Form("%s_%s",variable[iVariable].Data(),proc[iproc].Data()), 8, 0.5, 8.5);
-          double max(0.);
+          TH1D* dummy = new TH1D(Form("dummy%s_%s",variable[iVariable].Data(),proc[iproc].Data()), Form("%s_%s",variable[iVariable].Data(),proc[iproc].Data()), 8, 0.5, 8.5);
+          double max(0.), min(0.);
+          int maxbin(-1),minbin(-1);
           for(int ivar=1; ivar<9; ++ivar)
             {
               TH1* var = (TH1*) f->Get(Form("%s_%s_scale%d",variable[iVariable].Data(),proc[iproc].Data(),ivar));
               double rerr(0.);
               double r(fitRatio(var, nominal, rerr));
               //cout << r << endl;
-              if(r>max) max=r;
+              if(r>max){ max=r; maxbin=ivar;}
+              if(r<min){ min=r; minbin=ivar;}
               gr->SetBinContent(ivar,r);
               gr->SetBinError(ivar,rerr);
             }
+          dummy->SetBinContent(maxbin,10*gr->GetMaximum());
+          dummy->SetBinContent(minbin,10*gr->GetMinimum());
           cout << max << endl;
           if(doScaleStudy)
             {
@@ -101,6 +116,19 @@ void makeBdtScalePDFVariations(TString dir, TString region, bool savePdfReplicas
               gr->GetXaxis()->SetBinLabel(7, "muR = 0.5, muF = 2");   
               gr->GetXaxis()->SetBinLabel(8, "muR = 0.5, muF = 0.5"); 
               gr->Write();
+              if(doFigures)
+                {
+                  TCanvas* c = new TCanvas("c","c", 800,800);
+                  c->cd();
+                  gr->Draw("axis");
+                  dummy->SetFillColor(kRed);
+                  dummy->SetFillStyle(3);
+                  dummy->Draw("same");
+                  gr->Draw("pesame");
+                  c->Print(Form("%s/%s/%s_scaleVar.png",dir.Data(),region.Data(),gr->GetName()));
+                  c->Print(Form("%s/%s/%s_scaleVar.pdf",dir.Data(),region.Data(),gr->GetName()));
+                  delete c;
+                }
             }
 
         }
