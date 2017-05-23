5faf35c0fc1170eed1d4dc5c6efca36a4ed9e057
Pietro Vischia <pietro.vischia@gmail.com>
2017-05-23 16:12:45 +0200

    Improved analysis script

---
+++ TTHAnalysis/python/plotter/wzsm/doAnal.py
@@ -0,0 +1,114 @@
+from utils import command as command
+from utils import clean
+from utils import os
+import string
+
+"""
+"""
+        # Regions:
+    # 
+    # C:         3lC         SR-42            OSSF + 1tau
+    # D:         3lD         SR-56            e mu tau
+    # E:         3lE         SR-70            SS + 1 tau
+    # F:         3lF         SR-80            A: OSSF, F: e/mu + 1tau
+    # I:         3lI         SR-96            3l + 1tau
+
+regions = {
+    'C':      ['3lC', 'SR-42', 'OSSF + 1tau'              ], 
+    'D':      ['3lD', 'SR-56', 'e mu tau'                 ],
+    'E':      ['3lE', 'SR-70', 'SS + 1 tau'               ],
+    'F':      ['3lF', 'SR-80', 'A: OSSF, F: e/mu + 1tau'  ],
+    'I':      ['4lI', 'SR-96', '3l + 1tau'                ],
+    'CRTAUH': ['CRTAUH', 'CRTAUH', 'Control regions tauh' ],
+    }
+
+"""
+"""
+
+import optparse
+# Command line options
+usage = 'usage: %prog [--newData]'
+parser = optparse.OptionParser(usage)
+parser.add_option('-i', '--input',          dest='inputDir',       help='input directory',        default='/pool/ciencias/HeppyTrees/RA7/estructura/trees_8011_July5_allscans/',           type='string')
+parser.add_option('-o', '--output',         dest='outputDir',      help='output directory',       default='~/www/susyRA7/',           type='string')
+parser.add_option('-a', '--action',         dest='action',         help='which action to perform', default='crtau', type='string')
+parser.add_option('-s', '--subaction',      dest='subaction',      help='which subAction to perform', default='', type='string')
+parser.add_option('-p', '--pretend',        dest='pretend',        help='only print commands out', action='store_true')
+
+(opt, args) = parser.parse_args()
+
+inputDir  = opt.inputDir
+outputDir = opt.outputDir
+action    = opt.action
+subaction = opt.subaction
+pretend   = opt.pretend
+blind = '--flags "-X blinding"'
+
+index="/nfs/fanae/user/vischia/www/index.php"
+
+def runPlots(cuts, mca, out, plots, inputDir, outputDir, pgroup, jei, lumi, mcc, mccother, trigdef, toplot, weights, functions, enablecuts, header):
+        clean(out)
+        os.system('mkdir -p {out}'.format(out=out))
+        os.system('cp {index} {outputDir}'.format(index=index,outputDir=out))
+        # --Fs {inputDir}/leptonJetReCleanerSusyEWK3L --Fs {inputDir}/leptonBuilderEWK 
+        daweights=''
+        if weights != '':
+                daweights=" -W '{weights}' ".format(weights=weights)
+        cmd = "python mcPlots.py {mca} {cuts} {plots} -P {inputDir} --Fs {inputDir}/leptonJetReCleanerWZSM --Fs {inputDir}/leptonBuilderWZSM --FMCs {inputDir}/bTagEventWeightFullSimWZ30 --pdir {outputDir}  -j {jei} -l {lumi} --s2v --tree treeProducerSusyMultilepton --mcc {mcc} {mccother} --mcc {trigdef} -f {daweights} {pgroup} --plotgroup fakes_appldata+=promptsub  --legendWidth 0.18 --legendFontSize 0.026 --showMCError -f {toplot} --showRatio --perBin --legendHeader \'{header}\' --maxRatioRange 0.5 2.0 --fixRatioRange --print C,pdf,png,txt --ratioOffset 0.03 {functions} {enablecuts} --env oviedo ".format(mca=mca,cuts=cuts,plots=plots,inputDir=inputDir,outputDir=out,pgroup=pgroup,jei=jei,lumi=lumi,mcc=mcc,mccother=mccother,trigdef=trigdef,daweights=daweights,toplot=toplot,functions=functions,enablecuts=enablecuts,header=header)
+        command(cmd, pretend)
+        os.system('cp {index} {outputDir}'.format(index=index,outputDir=out))
+
+
+def runCards(variable, binning, cuts, mca, out, plots, systs, inputDir, processes, signals, pgroup, outputDir, jei, lumi, mcc, mccother, trigdef, weights, functions, enablecuts):
+        # example var: SSR4bins
+        # example binning: '4,0.5,4.5'
+        os.system('mkdir -p {out}'.format(out=out))
+        daweights=''
+        if weights != '':
+                daweights=" -W '{weights}' ".format(weights=weights)
+        daprocesses=''
+        if processes != '':
+                daprocesses=" -p data,{processes} ".format(processes=processes)
+
+        cmd = "python makeShapeCardsSusy.py {mca} {cuts} {variable} '{binning}' {systs} -P {inputDir} --Fs {inputDir}/leptonBuilderEWK --Fs {inputDir}/leptonJetReCleanerSusyEWK2L -j {jei} -l {lumi} --s2v --tree treeProducerSusyMultilepton --mcc {mcc} --mcc {mccother} --mcc {trigdef} -f  {daweights} {functions} {daprocesses} {signals} {pgroup} --od {outputDir} --ms -o {variable} {enablecuts} ".format(mca=mca,cuts=cuts,variable=variable,binning=binning,systs=systs,inputDir=inputDir,daprocesses=daprocesses,signals=signals,pgroup=pgroup,jei=jei,lumi=lumi,mcc=mcc,mccother=mccother,trigdef=trigdef,daweights=daweights,functions=functions,outputDir=out,enablecuts=enablecuts)
+        command(cmd, pretend)
+        os.system('cp {index} {outputDir}'.format(index=index,outputDir=out))
+        
+######################################################################################
+
+
+
+
+if(action=='generic'):
+        print 'Now plotting generic plots'
+        plots='wzsm/plots_wzsm.txt'
+        mcc='wzsm/mcc_varsub_wzsm.txt'
+        mccother=''
+        trigdef='wzsm/mcc_triggerdefs.txt'
+        weights=' puw_nInt_Moriond(nTrueInt)*getLepSF(LepSel1_conePt,LepSel1_eta,LepSel1_pdgId,1)*getLepSF(LepSel2_conePt,LepSel2_eta,LepSel2_pdgId,1)*getLepSF(LepSel3_conePt,LepSel3_eta,LepSel3_pdgId,1)*bTagWeight '
+        functions=' --load-macro wzsm/functionsPUW.cc --load-macro wzsm/functionsSF.cc --load-macro wzsm/functionsWZ.cc '
+        toplot='--sP m3l,m3lmet,m3l_l,m3lmet_l '
+        if(subaction!=''):
+                toplot='--sP \'{toplot}\''.format(toplot=subaction)
+        if(subaction=='all'):
+                toplot=''
+        batch=' -q batch '
+        batch=''
+        direct=' --pretend '
+        direct=' '
+        jei='6'
+        jei='40'
+        # https://hypernews.cern.ch/HyperNews/CMS/get/physics-announcements/4495.html
+        lumi='35.867'
+        enablecuts=' '
+        pgroup=' --pgroup internal:=ttZ,Gstar,ZGi --pgroup external:=TTG,WG,ZG,TG,Gstare --pgroup incl_fakes_appldata+=incl_promptsub '
+        pgroup=' -p data -p prompt.* -p rares.* -p fakes_appldata --plotgroup fakes_appldata+=promptsub --neglist promptsub '
+        #
+        header='All'
+        cuts='wzsm/cuts_wzsm.txt'
+        mca='wzsm/mca_includes.txt'
+        out=outputDir+'test/'
+        runPlots(cuts, mca, out, plots, inputDir, outputDir, pgroup, jei, lumi, mcc, mccother, trigdef, toplot, weights, functions,enablecuts, header)
+        
+
+print 'Everything is done now'
--- TTHAnalysis/python/plotter/wzsm/doAnal.sh
+++ TTHAnalysis/python/plotter/wzsm/doAnal.sh
@@ -1,1 +1,33 @@
-mcPlots.py WZSM/mca_includes.txt WZSM/cuts_wzsm.txt WZSM/plots_wzsm.txt -P /pool/ciencias/HeppyTrees/RA7/estructura/treesM17 --Fs {P}/leptonJetReCleanerWZSM --Fs {P}/leptonBuilderWZSM --FMCs {P}/bTagEventWeightFullSimWZ30 --pdir /nfs/fanae/user/nachos/www/WZ2016/mWZ_varbin -j 24 -l 35.867 --s2v --tree treeProducerSusyMultilepton --mcc WZSM/mcc_varsub_wzsm.txt --mcc WZSM/mcc_triggerdefs.txt --legendWidth 0.18 --legendFontSize 0.026 -f --sP m3l_l,m3lmet_l --perBin -p data -p prompt.* -p rares.* -p fakes_appldata --plotgroup fakes_appldata+=promptsub --neglist promptsub -W puw_nInt_Moriond(nTrueInt)*getLepSF(LepSel1_conePt,LepSel1_eta,LepSel1_pdgId,1)*getLepSF(LepSel2_conePt,LepSel2_eta,LepSel2_pdgId,1)*getLepSF(LepSel3_conePt,LepSel3_eta,LepSel3_pdgId,1)*bTagWeight --showMCError --load-macro WZSM/functionsPUW.cc --load-macro WZSM/functionsSF.cc --load-macro WZSM/functionsWZ.cc --print C,pdf,png,txt --showRatio --ratioOffset 0.03 --maxRatioRange 0.5 2.0 --fixRatioRange --env oviedo
+#!/bin/bash
+
+ 
+INPUTDIR="/pool/ciencias/HeppyTrees/RA7/estructura/treesM17"
+TREESDIR=${INPUTDIR}                                                                                     
+OUTPUTDIR="/OBSOLETE"                                                                                    
+WEBDIR="/nfs/fanae/user/vischia/www/wz/"                                            
+
+#    mcPlots.py WZSM/mca_includes.txt WZSM/cuts_wzsm.txt WZSM/plots_wzsm.txt -P  --Fs {P}/leptonJetReCleanerWZSM --Fs {P}/leptonBuilderWZSM --FMCs {P}/bTagEventWeightFullSimWZ30 --pdir /nfs/fanae/user/nachos/www/WZ2016/mWZ_varbin -j 24 -l 35.867 --s2v --tree treeProducerSusyMultilepton --mcc WZSM/mcc_varsub_wzsm.txt --mcc WZSM/mcc_triggerdefs.txt --legendWidth 0.18 --legendFontSize 0.026 -f --sP m3l_l,m3lmet_l --perBin -p data -p prompt.* -p rares.* -p fakes_appldata --plotgroup fakes_appldata+=promptsub --neglist promptsub -W puw_nInt_Moriond(nTrueInt)*getLepSF(LepSel1_conePt,LepSel1_eta,LepSel1_pdgId,1)*getLepSF(LepSel2_conePt,LepSel2_eta,LepSel2_pdgId,1)*getLepSF(LepSel3_conePt,LepSel3_eta,LepSel3_pdgId,1)*bTagWeight --showMCError --load-macro WZSM/functionsPUW.cc --load-macro WZSM/functionsSF.cc --load-macro WZSM/functionsWZ.cc --print C,pdf,png,txt --showRatio --ratioOffset 0.03 --maxRatioRange 0.5 2.0 --fixRatioRange --env oviedo
+
+    
+if [ "$1" == "plot" ]; then
+    
+    ACTION=""
+    SUBACTION=""
+    # ACTION can be general
+    
+    if [ "$2" == "" ]; then
+        echo "ACTION is empty. It can be generic"
+        exit -1
+    else
+        ACTION=" -a ${2}"
+        if [ "$3" != "" ]; then
+            SUBACTION=" -s ${3} "
+        fi
+    fi
+    
+    PRETEND=" --pretend  "
+    PRETEND=""
+    python wzsm/doAnal.py -i ${INPUTDIR} -o ${WEBDIR} ${ACTION} ${SUBACTION} ${PRETEND}
+
+fi
+exit 0
