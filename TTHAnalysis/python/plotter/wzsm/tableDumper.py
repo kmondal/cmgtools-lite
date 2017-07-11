48baad260c150e79954fbae527fbf27db80d2536
Pietro Vischia <pietro.vischia@gmail.com>
2017-07-11 12:54:45 +0200

    Updated tableDumper

--- TTHAnalysis/python/plotter/wzsm/tableDumper.py
+++ TTHAnalysis/python/plotter/wzsm/tableDumper.py
@@ -11,14 +11,36 @@ parser.add_option('-o', '--output',         dest='outputDir',      help='output 
 parser.add_option('-a', '--action',         dest='action',         help='which action to perform', default='crtau', type='string')
 parser.add_option('-p', '--pretend',        dest='pretend',        help='only print commands out', action='store_true')
 parser.add_option('-s', '--signal',         dest='sigLabel',       help='label of signal in tables', default='WZ', type='string')
-parser.add_option('-w', '--workingPoint',   dest='wp', default='VT', help='working point for lepton ID (default is: VT)')
-
+parser.add_option('-w', '--workingPoint',   dest='wp',             help='working point for lepton ID (default is: VT)', default='' )
+parser.add_option('-r', '--region',         dest='reg',            help='region', default='srwz')
 (opt, args) = parser.parse_args()
 sigLabel=opt.sigLabel
 wp=opt.wp
+reg=opt.reg
 
-rawYields=readYieldsFromTable("/nfs/fanae/user/vischia/www/wz/wz/lepmva{wp}/srwz/m3lmet.txt".format(wp=wp))
 
-table = writeYieldsToLatex(rawYields,sigLabel)
+if wp=='':
+    print('------------------ VERY TIGHT MVA --------------------')
+    rawYields=readYieldsFromTable('/nfs/fanae/user/vischia/www/wz/wz/lepmvaVT/{reg}/m3lmet.txt'.format(reg=reg))
+    print writeYieldsToLatex(rawYields,sigLabel)
+    print('------------------ MEDIUM MVA ---------------------')
+    rawYields=readYieldsFromTable('/nfs/fanae/user/vischia/www/wz/wz/lepmvaM/{reg}/m3lmet.txt'.format(reg=reg))
+    print writeYieldsToLatex(rawYields,sigLabel)
+    print('------------------ TIGHT POG MVA ---------------------')
+    rawYields=readYieldsFromTable('/nfs/fanae/user/vischia/www/wz/wzpog/lepmvaVT/{reg}/m3lmet.txt'.format(reg=reg))
+    print writeYieldsToLatex(rawYields,sigLabel)
+    print('------------------ MEDIUM POG MVA ---------------------')
+    rawYields=readYieldsFromTable('/nfs/fanae/user/vischia/www/wz/wzpog/lepmvaM/{reg}/m3lmet.txt'.format(reg=reg))
+    print writeYieldsToLatex(rawYields,sigLabel)
+    print('------------------ TIGHT POG MVA MC ---------------------')
+    rawYields=readYieldsFromTable('/nfs/fanae/user/vischia/www/wz/wzMCpog/lepmvaVT/{reg}/m3lmet.txt'.format(reg=reg))
+    print writeYieldsToLatex(rawYields,sigLabel)
+    print('------------------ MEDIUM POG MVA MC ---------------------')
+    rawYields=readYieldsFromTable('/nfs/fanae/user/vischia/www/wz/wzMCpog/lepmvaM/{reg}/m3lmet.txt'.format(reg=reg))
+    print writeYieldsToLatex(rawYields,sigLabel)
+else:
+    rawYields=readYieldsFromTable('/nfs/fanae/user/vischia/www/wz/wz/lepmva{wp}/{reg}/m3lmet.txt'.format(reg=reg).format(wp=wp))
+    table = writeYieldsToLatex(rawYields,sigLabel)
+    print table
 
-print table
+
