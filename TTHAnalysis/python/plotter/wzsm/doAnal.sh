72da85253b120bec64fee54a1c14f2c196a4cc14
Carlos Erice <cericecid@gmail.com>
2017-12-01 17:38:41 +0100

    Pointed input directory to skimmed trees for quicker work

--- TTHAnalysis/python/plotter/wzsm/doAnal.sh
+++ TTHAnalysis/python/plotter/wzsm/doAnal.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
  
-INPUTDIR="/pool/ciencias/HeppyTrees/RA7/estructura/treesM17"
+#INPUTDIR="/pool/ciencias/HeppyTrees/RA7/estructura/treesM17" #Use skimmed (3LepGood, minMllAFAS > 4) instead
+INPUTDIR="/pool/ciencias/userstorage/carlosec/wzSkimmed/"
 TREESDIR=${INPUTDIR}                                                                                     
 OUTPUTDIR="/OBSOLETE"                                                                                    
-WEBDIR="/nfs/fanae/user/vischia/www/wz/"                                            
+WEBDIR="/nfs/fanae/user/carlosec/www/wz/"                                            
 
 #    mcPlots.py WZSM/mca_includes.txt WZSM/cuts_wzsm.txt WZSM/plots_wzsm.txt -P  --Fs {P}/leptonJetReCleanerWZSM --Fs {P}/leptonBuilderWZSM --FMCs {P}/bTagEventWeightFullSimWZ30 --pdir /nfs/fanae/user/nachos/www/WZ2016/mWZ_varbin -j 24 -l 35.867 --s2v --tree treeProducerSusyMultilepton --mcc WZSM/mcc_varsub_wzsm.txt --mcc WZSM/mcc_triggerdefs.txt --legendWidth 0.18 --legendFontSize 0.026 -f --sP m3l_l,m3lmet_l --perBin -p data -p prompt.* -p rares.* -p fakes_appldata --plotgroup fakes_appldata+=promptsub --neglist promptsub -W puw_nInt_Moriond(nTrueInt)*getLepSF(LepSel1_conePt,LepSel1_eta,LepSel1_pdgId,1)*getLepSF(LepSel2_conePt,LepSel2_eta,LepSel2_pdgId,1)*getLepSF(LepSel3_conePt,LepSel3_eta,LepSel3_pdgId,1)*bTagWeight --showMCError --load-macro WZSM/functionsPUW.cc --load-macro WZSM/functionsSF.cc --load-macro WZSM/functionsWZ.cc --print C,pdf,png,txt --showRatio --ratioOffset 0.03 --maxRatioRange 0.5 2.0 --fixRatioRange --env oviedo
 
@@ -55,7 +56,8 @@ elif [ "$1" == "VT" ]; then
     sh wzsm/doAnal.sh plot srwz VT
     sh wzsm/doAnal.sh plot ttcr VT
     sh wzsm/doAnal.sh plot dycr VT
-    
+    sh wzsm/doAnal.sh plot llnot VT 
+
 elif [ "$1" == "M" ]; then
     sh wzsm/doAnal.sh plot srwz M
     sh wzsm/doAnal.sh plot ttcr M
@@ -106,7 +108,7 @@ elif [ "$1" == "plot" ]; then
     fi
     
     PRETEND=" --pretend  "
-    PRETEND=""
+#    PRETEND=""
     python wzsm/doAnal.py -i ${INPUTDIR} -o ${WEBDIR} $STUFF ${WP} ${ACTION} ${SUBACTION} ${PRETEND}
 
 elif [ "$1" == "fr" ]; then
