f8cf3c5f93cc920130c19f2cf0d8ff2dec88a5e9
Pietro Vischia <pietro.vischia@gmail.com>
2018-02-20 15:45:12 +0100

    Align bias scale to the latest run

--- TTHAnalysis/python/plotter/wzsm/runUnfold.py
+++ TTHAnalysis/python/plotter/wzsm/runUnfold.py
@@ -34,13 +34,13 @@ def get_list_of_jobs():
         ret.append('python wzsm/unfold.py -i /nfs/fanae/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/wz_unfolding -f {fs} -b 1.0 -o unfold_1p0bias_noconstraintarea/{fs}/data -c common/WZSR.input.root -r >& logs/1p0bias_noconstraintarea_data_{fs}.log'.format(fs=fs))
         ret.append('python wzsm/unfold.py -i /nfs/fanae/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/wz_unfolding -f {fs} -b 1.0 -o unfold_1p0bias_noconstraintarea/{fs}/mcclosure -c common/WZSR.input.root -r --closure >& logs/1p0bias_noconstraintarea_mcclosure_{fs}.log'.format(fs=fs))
 
-        # Bias 1.11, area constraint
-        ret.append('python wzsm/unfold.py -i /nfs/fanae/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/wz_unfolding -f {fs} -b 1.11 -a -o unfold_1p11bias/{fs}/data -c common/WZSR.input.root -r >& logs/1p11bias_data_{fs}.log'.format(fs=fs))
-        ret.append('python wzsm/unfold.py -i /nfs/fanae/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/wz_unfolding -f {fs} -b 1.11 -a -o unfold_1p11bias/{fs}/mcclosure -c common/WZSR.input.root -r --closure >& logs/1p11bias_mcclosure_{fs}.log'.format(fs=fs))
+        # Bias 1.13, area constraint
+        ret.append('python wzsm/unfold.py -i /nfs/fanae/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/wz_unfolding -f {fs} -b 1.13 -a -o unfold_1p11bias/{fs}/data -c common/WZSR.input.root -r >& logs/1p11bias_data_{fs}.log'.format(fs=fs))
+        ret.append('python wzsm/unfold.py -i /nfs/fanae/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/wz_unfolding -f {fs} -b 1.13 -a -o unfold_1p11bias/{fs}/mcclosure -c common/WZSR.input.root -r --closure >& logs/1p11bias_mcclosure_{fs}.log'.format(fs=fs))
 
         # Bias 1.11, no area constraint
-        ret.append('python wzsm/unfold.py -i /nfs/fanae/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/wz_unfolding -f {fs} -b 1.11 -o unfold_1p11bias_noconstraintarea/{fs}/data -c common/WZSR.input.root -r >& logs/1p11bias_noconstraintarea_data_{fs}.log'.format(fs=fs))
-        ret.append('python wzsm/unfold.py -i /nfs/fanae/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/wz_unfolding -f {fs} -b 1.11 -o unfold_1p11bias_noconstraintarea/{fs}/mcclosure -c common/WZSR.input.root -r --closure >& logs/1p11bias_noconstraintarea_mcclosure_{fs}.log'.format(fs=fs))
+        ret.append('python wzsm/unfold.py -i /nfs/fanae/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/wz_unfolding -f {fs} -b 1.13 -o unfold_1p11bias_noconstraintarea/{fs}/data -c common/WZSR.input.root -r >& logs/1p11bias_noconstraintarea_data_{fs}.log'.format(fs=fs))
+        ret.append('python wzsm/unfold.py -i /nfs/fanae/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/wz_unfolding -f {fs} -b 1.13 -o unfold_1p11bias_noconstraintarea/{fs}/mcclosure -c common/WZSR.input.root -r --closure >& logs/1p11bias_noconstraintarea_mcclosure_{fs}.log'.format(fs=fs))
 
     return ret
 
