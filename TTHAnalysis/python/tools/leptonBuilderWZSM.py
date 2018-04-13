f985e3b9eec4d9fabbb833b9356dab4e4e288c68
Pietro Vischia <pietro.vischia@gmail.com>
2018-04-13 13:21:52 +0200

    Fix whiteline

--- TTHAnalysis/python/tools/leptonBuilderWZSM.py
+++ TTHAnalysis/python/tools/leptonBuilderWZSM.py
@@ -31,7 +31,6 @@ class OSpair:
     ## __init__
     ## _______________________________________________________________
     def __init__(self, l1, l2):
-
         self.l1   = l1
         self.l2   = l2
         self.load()
